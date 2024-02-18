e53067462323ec2c5f45a40953d53195d73ed59b
Roberto Junior <robertolucenav@gmail.com>
2024-02-18 10:06:41 -0300

    Update
    Retirado funções que não usei e comentários

--- scrape.py
+++ scrape.py
@@ -5,37 +5,17 @@ import requests
 
 def pegar_dados(url):
     response = requests.get(url)
-
-    # Utiliza para fazer a analise e retorno em formato de texto
     soup = BeautifulSoup(response.text, 'lxml')
-
-    # Retorna uma lista com toda as informações sobre o livro, pesquisando em todas as article class='product_pod'
     livros = soup.find_all('article', class_='product_pod')
 
     dados = []
 
-    # Localizar informacoes especificas na pagina, cada livro conta como um dado
     for livro in livros:
         item = {}
-        # Procura imagens dentro do article, e retorna o alt para armazenar como titulo
         item['Titulo'] = livro.find('img', class_='thumbnail').attrs['alt']
-        # Procura valor do livro, e pula o primeiro caractere para nao pegar cifrao
         item['Preco'] = livro.find('p', class_='price_color').text[1:]
-
-        # Adiciona o dado a lista de dados
         dados.append(item)
-
     return dados
-
-
-"""def proxima_pagina():
-
-    for numero_pagina in range (1,51):
-
-        url = f'https://books.toscrape.com/catalogue/page-{numero_pagina}.html'
-        response = requests.get(url)
-
-    return pagina"""
 
 
 def exportar_dados(dados):
